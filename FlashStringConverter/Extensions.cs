33f49be4af6545a443eef3f049de4962c200ea88
Szántó Benjámin <61885011+vellt@users.noreply.github.com>
2021-10-02 20:47:51 +0200

    First commit

---
+++ FlashStringConverter/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FlashStringConverter
+{
+    public static class Extensions
+    {
+        public static int? ToInt32(this string o)
+        {
+            int? result = null;
+            try { result = Convert.ToInt32(o); }
+            catch (Exception) { }
+            return result;
+        }
+        public static double? ToDouble(this string o)
+        {
+            double? result = null;
+            try { result = Convert.ToDouble(o); }
+            catch (Exception) { }
+            return result;
+        }
+        public static long? ToInt64(this string o)
+        {
+            long? result = null;
+            try { result = Convert.ToInt32(o); }
+            catch (Exception) { }
+            return result;
+        }
+        public static DateTime? ToDateTime(this string o)
+        {
+            DateTime? result = null;
+            try { result = Convert.ToDateTime(o); }
+            catch (Exception) { }
+            return result;
+        }
+        public static bool? ToBoolean(this string o)
+        {
+            bool? result = null;
+            try { result = Convert.ToBoolean(o); }
+            catch (Exception) { }
+            return result;
+        }
+    }
+}
